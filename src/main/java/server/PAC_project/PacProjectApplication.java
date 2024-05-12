fd9f38b3b2867aeb44cd728666df2c2b989cd20f
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-05-12 14:18:58 +0900

    first commit

---
+++ src/main/java/server/PAC_project/PacProjectApplication.java
@@ -0,0 +1,13 @@
+package server.PAC_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/PAC_project/PacProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package server.PAC_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
