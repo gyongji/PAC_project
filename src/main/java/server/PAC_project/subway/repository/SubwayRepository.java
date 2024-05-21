febebd7fc1cd09d797defc2bb1961398d226928f
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-05-21 12:29:34 +0900

    fix: SubwayLine Entity integration

--- src/main/java/server/PAC_project/subway/repository/SubwayRepository.java
+++ src/main/java/server/PAC_project/subway/repository/SubwayRepository.java
@@ -1,9 +1,11 @@
 package server.PAC_project.subway.repository;
 
-import server.PAC_project.subway.model.entity.LineFive;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
+import server.PAC_project.subway.model.entity.Line;
 
-
-public interface SubwayRepository extends JpaRepository<LineFive, Integer> {
+@Repository
+public interface SubwayRepository extends JpaRepository<Line, Long> {
 
 }
