89a832819476583b4649947a7236787c3ff352e4
LeeJongWon <106072775+LJW22222@users.noreply.github.com>
2024-05-31 18:28:29 +0900

    feat -> Scheduling 활성화 및 타임존 변경

--- src/main/java/server/PAC_project/PacProjectApplication.java
+++ src/main/java/server/PAC_project/PacProjectApplication.java
@@ -2,12 +2,17 @@ package server.PAC_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
+@EnableScheduling
 @SpringBootApplication
 public class PacProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PacProjectApplication.class, args);
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
