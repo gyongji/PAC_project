92cb9070cc64392261851619bf4dc2768e5a2ff1
yunji <aka130613@gmail.com>
2024-06-09 18:42:55 +0900

    refactor : Name change

--- src/main/java/server/PAC_project/bus/model/dto/ResponseBusRouteDTO.java
+++ src/main/java/server/PAC_project/bus/model/dto/ResponseBusRouteDTO.java
@@ -1,4 +1,4 @@
-package server.PAC_project.bus.model.dto;
+package server.PAC_project.bus.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class ResponseBusRouteDTO {
     @JsonProperty("ROUTE")
-    private String route;
+    private String ROUTE_NAME;
     @JsonProperty("ROUTE_ID")
     private String ROUTE_ID;
 }
