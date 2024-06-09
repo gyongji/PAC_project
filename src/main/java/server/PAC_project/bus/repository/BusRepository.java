8446101f25d2d73ca3c26d9296dc7bb35e931428
yunji <aka130613@gmail.com>
2024-06-09 19:05:25 +0900

    feat: Create BusRepository interface for bus entity persistence
    - Define BusRepository interface extending JpaRepository for CRUD operations on BusEntity.

---
+++ src/main/java/server/PAC_project/bus/repository/BusRepository.java
@@ -0,0 +1,9 @@
+package server.PAC_project.bus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.PAC_project.bus.model.entity.BusEntity;
+
+
+public interface BusRepository extends JpaRepository<BusEntity, Long> {
+
+}
