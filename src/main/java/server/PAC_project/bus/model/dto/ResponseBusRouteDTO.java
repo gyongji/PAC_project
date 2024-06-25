7152ad9b830aec605e92a8c528995aaac2c6954e
yunji <aka130613@gmail.com>
2024-06-25 16:26:43 +0900

    feat: Add ResponseBusRouteDTO for bus route responses
    - Added ResponseBusRouteDTO class to handle bus route response data.
    - Included fields for route name and in/out code.
    - Added a builder for convenient object creation.

--- src/main/java/server/PAC_project/bus/model/dto/ResponseBusRouteDTO.java
+++ src/main/java/server/PAC_project/bus/model/dto/ResponseBusRouteDTO.java
@@ -1,17 +1,18 @@
 package server.PAC_project.bus.model.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Data;
-import server.PAC_project.bus.model.entity.BusEntity;
 
 @Data
 public class ResponseBusRouteDTO {
-    @JsonProperty("ROUTE")
-    private String ROUTE_NAME;
-    @JsonProperty("ROUTE_ID")
-    private String ROUTE_ID;
 
-    public BusEntity toEntity() {
-        return BusEntity.toEn(this);
+    private String ROUTENAME;
+    private String INOUT_CODE;
+
+    @Builder
+    public ResponseBusRouteDTO(String ROUTENAME, String INOUT_CODE) {
+        this.ROUTENAME = ROUTENAME;
+        this.INOUT_CODE = INOUT_CODE;
     }
+
 }
