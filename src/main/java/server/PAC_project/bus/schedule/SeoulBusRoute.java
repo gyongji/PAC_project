eee7c4c473bcdd089d54a936a8239b6d8adbf50d
yunji <aka130613@gmail.com>
2024-06-25 16:38:41 +0900

    refactor: overhaul code structure and enhance JSON parsing
    - Implemented `parseBusExcel` method to parse bus route information from Excel file
    - Modified `getData` scheduling method for fetching Seoul bus route IDs
    - Added logic in `parser` method to parse JSON data and extract specific fields, converting them to a list of `FinalBusDTO`
    - Adjusted dependency injection and variable initialization

--- src/main/java/server/PAC_project/bus/schedule/SeoulBusRoute.java
+++ src/main/java/server/PAC_project/bus/schedule/SeoulBusRoute.java
@@ -6,8 +6,7 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.scheduling.annotation.Scheduled;
-import server.PAC_project.bus.model.dto.ResponseBusRouteDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import server.PAC_project.bus.model.dto.FinalBusDTO;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,6 +21,7 @@ import server.PAC_project.bus.util.BusMapto;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -53,14 +53,15 @@ public class SeoulBusRoute {
 
     // 서울특별시 한정 버스 노선 ID값 조회 서비스
     @Scheduled(cron = "0 0 03 25 * ?")
-    public void getData() throws JsonProcessingException {
-       List<BusEntity> BusEntitys = BusMapto.mapBusToEtity(parser());
-       busRepository.saveAll(BusEntitys);
-    }
+    public void getData() throws IOException {
+        List<BusEntity> busEntities = BusMapto.mapBusToEtity(parser());
+        busRepository.saveAll(busEntities);
+        }
 
     // Exel Parsing
-    public Map<String, String> parseBusExcel(String busRouteName) throws IOException {
-        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("bus/v0.3_기후동행카드_이용노선도_정리_240513.xlsx");
+    public Map<String, String> parseBusExcel() throws IOException {
+        InputStream inputStream
+                = Thread.currentThread().getContextClassLoader().getResourceAsStream("bus/v0.3_기후동행카드_이용노선도_정리_240513.xlsx");
         Map<String, String> busRouteInformationList = new HashMap<>();
         if (inputStream == null) {
             throw new IOException("File ERROR 404");
@@ -68,21 +69,25 @@ public class SeoulBusRoute {
 
         XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
 
-        XSSFSheet sheet = workbook.getSheet(busRouteName);
+        // 엑셀 시트 이름 콘솔에 출력
+        System.out.println("Name :: " + workbook.getSheetName(1));
+
+        // 엑셀 시트에서 버스 이름 가져오기
+        XSSFSheet sheet = workbook.getSheetAt(1);
         if(sheet != null) {
             int rowIndex;
 
             int physicalNumberOfRows = sheet.getPhysicalNumberOfRows();
-            for (rowIndex = 5; rowIndex < physicalNumberOfRows; rowIndex++) {
+            for (rowIndex = 6; rowIndex < physicalNumberOfRows; rowIndex++) {
 
                 XSSFRow row = sheet.getRow(rowIndex);
                 if (row != null) {
-                    XSSFCell cell1 = row.getCell(0); // Assuming the bus route ID is stored in the first column
-                    XSSFCell cell3 = row.getCell(1); // Assuming the bus route name is stored in the second column
+                    XSSFCell cell1 = row.getCell(10); // Assuming the bus route ID is stored in the first column
+                    XSSFCell cell3 = row.getCell(12); // Assuming the bus route name is stored in the second column
                     if (cell1 == null || cell3 == null) {
                         break;
                     }
-                    if (row.getCell(0).getCellType() == CellType.BLANK) {
+                    if (row.getCell(10).getCellType() == CellType.BLANK) {
                         break;
                     }
                     busRouteInformationList.put(cell1.toString(), cell3.toString());
@@ -93,8 +98,10 @@ public class SeoulBusRoute {
     }
 
     // 정보 데려오는 부분
-    private List<ResponseBusRouteDTO> parser() throws JsonProcessingException {
+    private List<FinalBusDTO> parser() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> stringStringMap = parseBusExcel();
+        List<FinalBusDTO> busEntities = new ArrayList<>();
         String STATION_COORDINATES_URL = seoulBusRouteUrl
                 + seoulBusRouteKey
                 + seoulBusRouteEndPoint
@@ -110,9 +117,21 @@ public class SeoulBusRoute {
         JsonNode jsonNode1 = objectMapper.readTree(dataList);
         jsonNode1 = jsonNode1.get("busRoute");
         jsonNode1 = jsonNode1.get("row");
+        for (JsonNode jsonNode : jsonNode1) {
+            FinalBusDTO finalbusDTO = objectMapper.treeToValue(jsonNode, FinalBusDTO.class);
+            for (String s : stringStringMap.keySet()) {
+                if (jsonNode.get("ROUTE").asText().equals(s)) {
+                    finalbusDTO.setINOUT_CODE(stringStringMap.get(s));
+                    break;
+                }
+                finalbusDTO.setINOUT_CODE("200");
+            }
+            busEntities.add(finalbusDTO);
+        }
         System.out.println(jsonNode1);
-
+        objectMapper.readValue(jsonNode1.toString(), new TypeReference<>() {});
         // JSON 데이터를 LIST<ResponseBusRouteDTO> 형태로 변환
-        return objectMapper.readValue(jsonNode1.toString(), new TypeReference<>() {});
+        return busEntities;
     }
+
 }
