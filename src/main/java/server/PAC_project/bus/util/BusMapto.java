844cc4424b7aefa22fd936d29061ee80e0cd7aba
yunji <aka130613@gmail.com>
2024-07-04 11:20:10 +0900

    feat: Refactored mapping logic in BusMapto class
    - Updated mapBusToFinalBusDTO method to convert BusEntity objects to FinalBusDTO objects, preserving ROUTEID, ROUTENAME, and INOUT_CODE fields.

--- src/main/java/server/PAC_project/bus/util/BusMapto.java
+++ src/main/java/server/PAC_project/bus/util/BusMapto.java
@@ -4,6 +4,7 @@ import server.PAC_project.bus.model.dto.FinalBusDTO;
 import server.PAC_project.bus.model.dto.ResponseBusRouteDTO;
 import server.PAC_project.bus.model.entity.BusEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,18 @@ public class BusMapto {
                 .ROUTENAME(busEntity.getROUTENAME())
                 .INOUT_CODE(busEntity.getINOUT())
                 .build();
+    }
+
+    public static List<FinalBusDTO> mapBusToFinalBusDTO(List<BusEntity> busEntities) {
+        List<FinalBusDTO> finalBusDTOs = new ArrayList<>();
+        for (BusEntity busEntity : busEntities) {
+            FinalBusDTO finalBusDTO = new FinalBusDTO();
+            finalBusDTO.setROUTEID(busEntity.getROUTEID());
+            finalBusDTO.setROUTENAME(busEntity.getROUTENAME());
+            finalBusDTO.setINOUT_CODE(busEntity.getINOUT());
+            finalBusDTOs.add(finalBusDTO);
+        }
+        return finalBusDTOs;
     }
 
 }
